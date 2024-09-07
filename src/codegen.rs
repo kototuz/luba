1e75e5147c2d760bfeb36e10315b131079ca7455
kototuz <ivann1812@gmail.com>
2024-09-07 23:36:42 +0300

    Programm accepts a source file

--- src/codegen.rs
+++ src/codegen.rs
@@ -61,20 +61,30 @@ impl Datapack {
 
 pub fn gen_dp<'a>(
     dp_name: &'a str,
-    _exprs: &Vec<parser::Expr>,
+    exprs: &Vec<parser::Expr>,
     stmts: &Vec<parser::Stmt>
 ) {
-    use crate::parser;
-
-    // 1+2+3+4+5
-    // 1+2+3+4*5 => 4*5 + 1+2+3+4
-
-    let dp = Datapack::create(dp_name);
     for stmt in stmts {
         match stmt {
             parser::Stmt::VarAssign { name, expr } => {
-            },
-            _ => unreachable!()
+                println!("var `{name}`");
+                for x in expr.clone() {
+                    println!("{:?}", exprs[x]);
+                }
+                println!("");
+            }
         }
     }
+    //use crate::parser;
+    //use std::io::prelude::*;
+    //
+    //let dp = Datapack::create(dp_name);
+    //let mut fn_file = dp.create_mcfn_file("test");
+    //let parser::Stmt::VarAssign { name, expr } = &stmts[0];
+    //let r = expr.clone();
+    //for x in r {
+    //    println!("{:?}", exprs[x]);
+    //    fn_file.write_all(b"scoreboard objectives add v0 dummy").expect("Could not write");
+    //    fn_file.write_all(b"");
+    //}
 }
--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,31 @@ mod lexer;
 mod parser;
 mod codegen;
 
+use std::process::exit;
+use std::io::prelude::*;
+
 fn main() {
-    let source = "num1 = 324 + 2 + 3 + 5 + 6; num2 = 234;";
-    //let mut lexer = lexer::Lexer::new(source);
-    //
-    //for x in lexer.by_ref() {
-    //    println!("{}", x);
-    //}
+    let file_path = std::env::args().nth(1).unwrap_or_else(|| {
+        eprintln!("ERROR: source file must be provided");
+        exit(1);
+    });
 
-    let (exprs, stmts) = parser::parse(&mut lexer::Lexer::new(source));
-    //println!("{exprs:?}");
-    //println!("{stmts:?}");
+    let mut src_file = std::fs::File::open(&file_path).unwrap_or_else(|err| {
+        eprintln!("ERROR: could not open file `{file_path}`: {err}");
+        exit(1);
+    });
 
-    codegen::gen_dp("test", &exprs, &stmts);
+    let mut buffer = String::new();
+    let _ = src_file.read_to_string(&mut buffer).inspect_err(|err| {
+        eprintln!("ERROR: could not read file `{file_path}`: {err}");
+        exit(1);
+    });
+
+    use std::path::PathBuf;
+    let file_name = PathBuf::from(PathBuf::from(file_path).file_name().unwrap());
+    let file_name = file_name.file_stem().unwrap();
+
+    let (mut exprs, mut stmts) = parser::parse(&mut lexer::Lexer::new(&buffer));
+
+    codegen::gen_dp(file_name.to_str().unwrap(), &mut exprs, &mut stmts);
 }
